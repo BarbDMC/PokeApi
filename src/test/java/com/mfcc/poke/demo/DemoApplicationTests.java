c06c7674310996d0b401868955cc5bde85bbba9b
BarbDMC <barbaradmorantesc@gmail.com>
2023-08-24 13:08:08 -0500

    Spring Boot project created

---
+++ src/test/java/com/mfcc/poke/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfcc.poke.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
